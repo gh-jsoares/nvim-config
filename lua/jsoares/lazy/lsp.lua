e9f385be269539f26f3399a0b2fbc882bba32aef
João Soares <jsoares.public@gmail.com>
2024-05-11 02:44:39 +0200

    Fixed keymappings for complete window

--- lua/jsoares/lazy/lsp.lua
+++ lua/jsoares/lazy/lsp.lua
@@ -80,8 +80,8 @@ return {
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         local cmp_mappings = cmp.mapping({
-            ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-            ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+            ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+            ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
             ["<C-y>"] = cmp.mapping.confirm({ select = true }),
             ["<C-Space>"] = cmp.mapping.complete(),
         })
